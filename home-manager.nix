b1868155895a7e2e2710f1471af464ffb3b144f2
conflagrate <jm@memorici.de>
2022-02-11 13:45:20 +0000

    Problem: Hard to juggle software between systems
    Solution:
    
     - Add a naive .local/bin export
     - Add a cruated and commented list of dependencies in a list which can be included into home-manager
    
    Flakes some day, yeah

---
+++ home-manager.nix
@@ -0,0 +1,20 @@
+{ pkgs, ...}:
+
+# This is a very very dumb function which, when given attrset with key "pkgs" shall
+
+[
+    pkgs.httpie
+    pkgs.jq
+    pkgs.curl
+    pkgs.imagemagick
+    # Stuff that hasn't been yet confirmed to work
+
+    ## Stuff that I don't know how to find
+    # pkgs.dd
+    # pkgs.pv
+    # Stuff that has to be externally configured
+    pkgs.gnupg
+    pkgs.darcs
+    ## Stuff that isn't yet implemented
+    # domaPakages.passveil
+]
