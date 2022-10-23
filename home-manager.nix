ffb5c196472d31c91af7721c293d1270836d6562
conflagrate <jm@memorici.de>
2022-10-23 14:53:50 +0100

    Update doma deps

--- home-manager.nix
+++ home-manager.nix
@@ -2,11 +2,16 @@
 
 # This is a very very dumb function which, when given attrset with key "pkgs" shall
 
+let npkgs = pkgs.nodePackages; in
 [
     pkgs.httpie
     pkgs.jq
     pkgs.curl
     pkgs.imagemagick
+    pkgs.nodejs-18_x
+    npkgs.typescript
+    npkgs.node-gyp
+    # npkgs.tsm
     # Stuff that hasn't been yet confirmed to work
 
     ## Stuff that I don't know how to find
@@ -18,4 +23,6 @@
     pkgs.tmux
     ## Stuff that isn't yet implemented
     # domaPakages.passveil
+    pkgs.elan
+    pkgs.go
 ]
